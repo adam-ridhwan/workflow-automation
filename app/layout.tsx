6b4ee2da5feaa619d6272b90f26f21b85f39715f
Adam Ridhwan <adamridhwan.1@gmail.com>
2026-07-31 19:57:33 -0400

    Fix incorrect import path for ConvexClientProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { ConvexClientProvider } from '@/components/ConvexClientProvider';
+import { ConvexClientProvider } from '@/components/convev-client-provider';
 import { ThemeProvider } from '@/components/theme-provider';
 import { ConvexAuthNextjsServerProvider } from '@convex-dev/auth/nextjs/server';
 import { Geist, Geist_Mono } from 'next/font/google';
