177d0bbba64085df2617c662cc244292121e2b47
Adam Ridhwan <adamridhwan.1@gmail.com>
2026-08-07 19:21:54 -0400

    fix loading

--- app/(app)/[workspaceName]/workflow/[workflowId]/loading.tsx
+++ app/(app)/[workspaceName]/workflow/[workflowId]/loading.tsx
@@ -1,9 +1,3 @@
 export default function WorkflowLoading() {
-  return (
-    <div
-      className='bg-canvas relative min-h-0 flex-1 overflow-hidden
-        [background-image:radial-gradient(var(--color-border)_1px,transparent_1px)]
-        [background-size:12px_12px]'
-    ></div>
-  );
+  return <div className='bg-canvas relative min-h-0 flex-1 overflow-hidden' />;
 }
